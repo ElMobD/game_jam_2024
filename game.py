fac7fdcf9410c60a247e961bb29e585abc6dd272
Mob <tracertraore@gmail.com>
2024-09-18 14:57:08 +0200

    Base du jeu

--- game.py
+++ game.py
@@ -3,103 +3,105 @@ import arcade
 # Dimensions de la fenêtre
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Jeu du Serpent avec Caméra Suivante"
+SCREEN_TITLE = "Carte avec fond PNG et caméra"
 
-# Constantes pour le serpent
-MOVEMENT_SPEED = 5
-SNAKE_WIDTH = 20
-SNAKE_HEIGHT = 20
+# Taille du fond (3 fois la taille de la fenêtre visible)
+MAP_WIDTH = SCREEN_WIDTH * 3
+MAP_HEIGHT = SCREEN_HEIGHT * 3
 
-# Largeur des bords
-BORDER_WIDTH = 10
+# Vitesse du joueur
+PLAYER_MOVEMENT_SPEED = 5
 
-class Snake(arcade.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.width = SNAKE_WIDTH
-        self.height = SNAKE_HEIGHT
-        self.color = arcade.color.GREEN
-        self.center_x = SCREEN_WIDTH // 2
-        self.center_y = SCREEN_HEIGHT // 2
-        self.change_x = 0
-        self.change_y = 0
-
-    def draw(self):
-        arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height, self.color)
-
-    def update(self):
-        self.center_x += self.change_x
-        self.center_y += self.change_y
-
-class SnakeGame(arcade.Window):
+class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        # Charger l'image de fond
+        self.background = arcade.load_texture("test.png")
         
-        # Crée une caméra pour suivre le serpent
+        # Créer le joueur
+        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png", scale=0.5)
+        self.player_sprite.center_x = SCREEN_WIDTH // 2
+        self.player_sprite.center_y = SCREEN_HEIGHT // 2
+        
+        # Créer une liste de sprites pour le joueur
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player_sprite)
+
+        # Configurer la caméra
         self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
-        
-        # Fond bleu ciel
-        arcade.set_background_color(arcade.color.SKY_BLUE)
-        
-        # Crée le serpent
-        self.snake = Snake()
 
     def on_draw(self):
+        """ Fonction d'affichage """
         arcade.start_render()
 
-        # Applique la caméra avant de dessiner
+        # Appliquer la caméra
         self.camera.use()
 
-        # Dessine les bords
-        arcade.draw_rectangle_filled(SCREEN_WIDTH // 2, BORDER_WIDTH // 2, SCREEN_WIDTH, BORDER_WIDTH, arcade.color.BLACK)  # Bord haut
-        arcade.draw_rectangle_filled(SCREEN_WIDTH // 2, SCREEN_HEIGHT - BORDER_WIDTH // 2, SCREEN_WIDTH, BORDER_WIDTH, arcade.color.BLACK)  # Bord bas
-        arcade.draw_rectangle_filled(BORDER_WIDTH // 2, SCREEN_HEIGHT // 2, BORDER_WIDTH, SCREEN_HEIGHT, arcade.color.BLACK)  # Bord gauche
-        arcade.draw_rectangle_filled(SCREEN_WIDTH - BORDER_WIDTH // 2, SCREEN_HEIGHT // 2, BORDER_WIDTH, SCREEN_HEIGHT, arcade.color.BLACK)  # Bord droit
-
-        # Dessine le serpent
-        self.snake.draw()
+        # Dessiner le fond (les coordonnées 0, 0 sont en bas à gauche)
+        arcade.draw_lrwh_rectangle_textured(0, 0, MAP_WIDTH, MAP_HEIGHT, self.background)
+        
+        # Dessiner le joueur
+        self.player_list.draw()
 
     def on_update(self, delta_time):
-        # Met à jour la position du serpent
-        self.snake.update()
+        """ Met à jour la logique du jeu """
+        self.player_list.update()
 
-        # Déplace la caméra pour qu'elle suive le serpent
+        # Restreindre le joueur à l'intérieur des limites de la carte
+        self.restrict_player_within_map()
+
+        # Mettre à jour la caméra pour suivre le joueur
         self.center_camera_to_player()
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.UP:
-            self.snake.change_y = MOVEMENT_SPEED
-            self.snake.change_x = 0
-        elif key == arcade.key.DOWN:
-            self.snake.change_y = -MOVEMENT_SPEED
-            self.snake.change_x = 0
-        elif key == arcade.key.RIGHT:
-            self.snake.change_x = MOVEMENT_SPEED
-            self.snake.change_y = 0
-        elif key == arcade.key.LEFT:
-            self.snake.change_x = -MOVEMENT_SPEED
-            self.snake.change_y = 0
+        """ Gérer les touches du clavier """
+        if key == arcade.key.W or key == arcade.key.UP:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.S or key == arcade.key.DOWN:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.A or key == arcade.key.LEFT:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.D or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
-        if key in [arcade.key.UP, arcade.key.DOWN]:
-            self.snake.change_y = 0
-        elif key in [arcade.key.LEFT, arcade.key.RIGHT]:
-            self.snake.change_x = 0
+        """ Gérer le relâchement des touches """
+        if key in [arcade.key.W, arcade.key.S, arcade.key.UP, arcade.key.DOWN]:
+            self.player_sprite.change_y = 0
+        elif key in [arcade.key.A, arcade.key.D, arcade.key.LEFT, arcade.key.RIGHT]:
+            self.player_sprite.change_x = 0
+
+    def restrict_player_within_map(self):
+        """ Empêcher le joueur de sortir de la carte (image de fond) """
+        # Limiter la position du joueur en fonction de la taille de la carte (image de fond)
+        if self.player_sprite.left < 0:
+            self.player_sprite.left = 0
+        if self.player_sprite.right > MAP_WIDTH:
+            self.player_sprite.right = MAP_WIDTH
+        if self.player_sprite.bottom < 0:
+            self.player_sprite.bottom = 0
+        if self.player_sprite.top > MAP_HEIGHT:
+            self.player_sprite.top = MAP_HEIGHT
 
     def center_camera_to_player(self):
-        # Définit la position où la caméra doit se centrer (autour du serpent)
-        screen_center_x = self.snake.center_x - (self.camera.viewport_width / 2)
-        screen_center_y = self.snake.center_y - (self.camera.viewport_height / 2)
+        """ Centrer la caméra sur le joueur """
+        # Déterminer le centre de la caméra en fonction de la position du joueur
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height / 2)
 
-        # Crée un vecteur pour la position de la caméra
-        camera_position = screen_center_x, screen_center_y
+        # Garder la caméra dans les limites de la carte
+        screen_center_x = max(0, min(screen_center_x, MAP_WIDTH - SCREEN_WIDTH))
+        screen_center_y = max(0, min(screen_center_y, MAP_HEIGHT - SCREEN_HEIGHT))
 
-        # Déplace la caméra à cette position
-        self.camera.move_to(camera_position)
+        # Appliquer la mise à jour de la position de la caméra
+        self.camera.move_to((screen_center_x, screen_center_y), 0.1)
 
-def main():
-    game = SnakeGame()
-    arcade.run()
+    def on_resize(self, width, height):
+        """ Ajuster la caméra lorsque la fenêtre est redimensionnée """
+        self.camera.resize(width, height)
 
+# Lancer le jeu
 if __name__ == "__main__":
-    main()
+    window = MyGame()
+    arcade.run()
