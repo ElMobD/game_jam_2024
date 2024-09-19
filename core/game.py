d085559624ce2ed6faa470a8db70501073ec66d3
ThibaultAnd261 <thibaultandwork@gmail.com>
2024-09-19 23:11:43 +0200

    Add a menu before the game

--- core/game.py
+++ core/game.py
@@ -4,10 +4,10 @@ from core.manager.viewManager import ViewManager
 
 class MyGame(arcade.Window):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(width=800, height=600, title="Mon Jeu")
         self.view_manager = ViewManager(self)
 
-    def setup(self):
-        # Lancer la première vue (map 1 par exemple)
+    def setup_game(self):
+        """ Méthode pour démarrer le jeu """
         initial_view = self.view_manager.create_new_view(map_id=1)
         self.show_view(initial_view)
